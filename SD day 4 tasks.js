fc36fff412fdd7e74a340354d65a69d5571fa2cd
Srajan <100074395+SrajanDongre@users.noreply.github.com>
2022-05-13 08:16:59 -0400

    Add files via upload

---
+++ SD day 4 tasks.js
@@ -0,0 +1,118 @@
+//(2-1) Squaring of a number
+var a = 3
+c= a**2
+console.log(c)
+
+//(2-2) Swapping 2 numbers
+let a=34;
+let b=67;
+let c;
+c=a;
+a=b;
+b=c;
+console.log('the value of a after swapping: ${a}');
+console.log('the value of b after swapping: ${b}');
+
+//(2-3) Addition of 2 numbers
+var x=12;
+var y=23;
+var z=34;
+d=x+y+z;
+console.log(d);
+
+//(2-4) Celcius to Fahrenheit conversion
+var e=5
+//value in degree ccelcius
+f=((9/5)*c)+32
+console.log('temp in fehrenheit is ',f)
+
+//(2-5) Meter to miles
+var m=10
+mi=m/1609.344
+console.log('meter to miles is',mi)
+
+//(2-6) Pounds to Kg
+var p=20
+Kg= p/2.20462
+console.log('Pound in kg is',Kg)
+
+//(2-7) Calculate Batting Average
+let PA=234;
+let LA=512;
+BA= PA/LA;
+console.log(BA);
+
+//(2-8) Calculate five test scores and print their average
+A=34;
+B=45;
+C=67;
+D=56;
+E=78;
+avg=(A+B+C+D+E)/5
+
+//(2-9) Power of any number x ^ y.
+X=3
+Y=4
+Z=X**Y
+console.log(Z)
+
+//(2-10) Calculate Simple Interest
+P=10000
+R=5
+T=2
+SI=(P*R*T)/100
+
+//(2-11) Calculate area of an equilateral triangle
+len=4
+area=((3**(1/2))/4)*(len**2)
+console.log(area)
+
+//(2-12) Area of isosceles triangle 
+let base=5
+let height=2
+ar=(1/2)*base*height
+console.log(ar)
+
+//(2-13) Volume of Sphere
+let r=5
+vol=(4/3)*(22/7)*(r**3)
+console.log(vol)
+
+//(2-14) Volume of Prism
+let Base=3
+let Height=4
+let Length=5
+v = (Base*Height*Length)/2
+
+//(2-15) Find area of a triangle
+let bas=4;
+let hei=3;
+Area = (1/2)*bas*hei
+console.log(Area)
+
+//(2-16) Give the Actual cost and Sold cost, Calculate Discount Of Product
+let AC=350
+let SC=50
+Dis= AC-SC;
+console.log(Dis)
+
+//(2-17) Given their radius of a circle and find its diameter, circumference and area.
+let R=5
+dia=2*R;
+cir=2*(22/7)*R;
+are=(22/7)*(R**2)
+
+//(2-18) Given two numbers and perform all arithmetic operations.
+let s=20
+let t=15
+sum= s+t;
+dif= s-t;
+mul= s*t;
+div= s/t;
+console.log(sum,dif,mul,div)
+
+//(2-19) Display the asterisk pattern as shown below(No loop needed):
+j='*'
+k= j*5
+
+
